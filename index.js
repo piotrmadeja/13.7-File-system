4f30f19fbff2c6999b7533c893e5816b9cee8b78
Piotr Madeja <piotr.madeja19@gmail.com>
2018-05-05 17:31:39 +0200

    13.7

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var colors = require('colors');
+
+fs.readdir('./', 'utf-8', function(err, files){
+	if(err) throw err;
+	var content = files;
+  
+  console.log('List of files in directory :'.cyan)
+
+	for(var i=0; i<content.length; i++){
+    var row = '\n' + content[i] + ', ';
+		console.log(row);
+		fs.appendFile('./Directory-content.txt', row , function(err){
+			if(err) throw err;
+			});
+	}
+	console.log('\nWriting file list to .txt'.green);
+});
+fs.writeFile('./Directory-content.txt...', ' ', function(err, data){
+  if(err) throw err;
+
+  console.log('\nDone!'.green);
+});
