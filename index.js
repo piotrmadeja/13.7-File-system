e1c8981265a809e29ccfdbf477e189dfb774544a
piotrmadeja <36596744+piotrmadeja@users.noreply.github.com>
2018-05-06 13:40:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var fs = require('fs');
 var colors = require('colors');
 
 fs.readdir('./', 'utf-8', function(err, files){
-	if(err) throw err;
-	var content = files;
-	console.log('List of files in directory :'.cyan)
+  if(err) throw err;
+  var content = files;
+  console.log('List of files in directory :'.cyan)
 	
-	for(var i=0; i<content.length; i++){
+  for(var i=0; i<content.length; i++){
     var row = '\n' + content[i] + ', ';
-		console.log(row);
-		fs.appendFile('./Directory-content.txt', row , function(err){
-			if(err) throw err;
-		});
-	}
-	console.log('\nWriting file list to .txt'.green);
+    console.log(row);
+    fs.appendFile('./Directory-content.txt', row , function(err){
+      if(err) throw err;
+    });
+  }
+  console.log('\nWriting file list to .txt'.green);
 });
 
 fs.writeFile('./Directory-content.txt...', ' ', function(err, data){
